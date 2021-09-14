17dd916d608648b2518f80dcbfad3a0607bc1c26
Ozan <cetino19@itu.edu.tr>
2021-09-14 21:38:03 +0300

    Services.js is gonna be implemented

--- Components/Services.js
+++ Components/Services.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import {mapsApi} from "react-native-maps"; 
+import tw from "tailwind-react-native-classnames"; 
+import {useSelector, useDispatch} from 'react-redux';
+import {selectOrigin, selectDestination} from "../slices/navSlice"; 
+import {Marker} from "react-native-maps"; 
+import MapViewDirections from 'react-native-maps-directions';
+import {GOOGLE_MAPS_APIKEY} from "@env"; 
+
 
 const Services = () => {
     return (
